817cdd2987780ce852c2b5356d54a4a762c57fcd
phi-cor <philippe.coratti.pro@gmail.com>
2021-01-30 13:05:27 +0100

    initialization

---
+++ src/components/contact-list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class ContactList extends React.Component {
+ 
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+ Liste
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ContactList from './components/contact-list';
+
+class Board extends React.Component {
+ 
+ contactList=new ContactList();
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      
+      <div>
+ <ContactList/>
+      </div>
+      
+    );
+  }
+}
+
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Board />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
